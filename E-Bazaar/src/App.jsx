dc79f62dc4cf9595736d0aaae357ea1fe245255e
Mohak Chauhan <mohak46812@gmail.com>
2023-03-26 01:01:51 +0530

    Components updated

--- E-Bazaar/src/App.jsx
+++ E-Bazaar/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Services from './components/Services'
+import Contact from './components/Contact'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
+      <Services/>
+      <Contact/>
     </div>
   )
 }
