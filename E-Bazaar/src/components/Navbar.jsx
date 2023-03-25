21ef39e4f0117cf7e23bcfc9f7e04e7b94831a7e
Gunanidhi <gunanidhitrivedi01@gmail.com>
2023-03-26 01:01:03 +0530

    changes in nevbar

--- E-Bazaar/src/components/Navbar.jsx
+++ E-Bazaar/src/components/Navbar.jsx
@@ -4,10 +4,19 @@ const Navbar = () => {
   return (
     <div>
       <div>
-        <nav class="navbar navbar-light bg-light">
-          <a class="navbar-brand" href="#">
-            <img src="E-Bazaar\public\logo.png" width="30" height="30" class="d-inline-block align-top" alt=""></img>
-          </a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div class="navbar-nav">
+              <a class="nav-item nav-link" href="#">Home </a>
+              <a class="nav-item nav-link" href="#">Farmer</a>
+              <a class="nav-item nav-link" href="#">Market Place</a>
+              <a class="nav-item nav-link" href="#">Contact</a>
+            </div>
+          </div>
         </nav>
       </div>
     </div>
