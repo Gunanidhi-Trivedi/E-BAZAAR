74bcbefd14a4d4745bb48879ecc1ce381e160d59
Gunanidhi <gunanidhitrivedi01@gmail.com>
2023-03-26 00:31:55 +0530

    created nevbar

--- E-Bazaar/src/components/Navbar.jsx
+++ E-Bazaar/src/components/Navbar.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-        <div>
-          Navbar
-        </div>
+      <div>
+        <nav class="navbar navbar-light bg-light">
+          <a class="navbar-brand" href="#">
+            <img src="E-Bazaar\public\logo.png" width="30" height="30" class="d-inline-block align-top" alt=""></img>
+          </a>
+        </nav>
+      </div>
     </div>
   )
 }
