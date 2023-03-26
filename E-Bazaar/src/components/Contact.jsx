dd2c380a7847ba11582150a60d139314f6c202a1
Mohak Chauhan <mohak46812@gmail.com>
2023-03-26 16:38:32 +0530

    connected nav links

--- E-Bazaar/src/components/Contact.jsx
+++ E-Bazaar/src/components/Contact.jsx
@@ -3,7 +3,7 @@ import './contact.css'
 
 const Contact = () => {
   return (
-    <div className='footer'>
+    <div className='footer' id='contact'>
       <h6>Created By - TechArmy</h6>
       <h6>Contact Us - <span><a href="#">techarmy@gmail.com</a></span></h6>
     </div>
--- E-Bazaar/src/components/Navbar.jsx
+++ E-Bazaar/src/components/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = () => {
           <div>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-              <a class="nav-item nav-link text-white px-4" href="#">HOME</a>
-              <a class="nav-item nav-link text-white px-4" href="#">SERVICES</a>
-              <a class="nav-item nav-link text-white px-4" href="#">CONTACT</a>
-              <a class="nav-item nav-link text-white px-4" href="#">LOGIN</a>
+              <a class="nav-item nav-link text-white px-4" href="#home">HOME</a>
+              <a class="nav-item nav-link text-white px-4" href="#services">SERVICES</a>
+              <a class="nav-item nav-link text-white px-4" href="#contact">CONTACT</a>
+              <a class="nav-item nav-link text-white px-4" href="#login">LOGIN</a>
             </div>
           </div>
           </div>
