d67700dd998ce3a8727f6b07de60e712c5979ccf
Gunanidhi <gunanidhitrivedi01@gmail.com>
2023-03-26 16:57:15 +0530

    error resolve

--- E-Bazaar/src/components/comp1/Services.jsx
+++ E-Bazaar/src/components/comp1/Services.jsx
@@ -3,38 +3,10 @@ import './services.css'
 
 const Services = () => {
   return (
-<<<<<<< HEAD
-
-    <div>
-      <div>
-        <h2 class = "text-center pt-4">SERVICES</h2>
-      </div>
-      <div className='parent '>
-        <div class="px-3">
-          <div class="card">
-            <h3 class="card-header text-center ">Farmer</h3>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-        <div class="px-3">
-          <div class="card ">
-            <h3 class="card-header text-center">Buyer</h3>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-          </div>
-        </div>
-      </div>
-=======
+    
     <div id='services'>
       Services
->>>>>>> dd2c380a7847ba11582150a60d139314f6c202a1
+
     </div>
   )
 }
