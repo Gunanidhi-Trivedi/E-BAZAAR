73b1755b25444244e8a6f7b1cd5142ba59568129
Gunanidhi <gunanidhitrivedi01@gmail.com>
2023-03-26 17:15:27 +0530

    created folders for farmer and buyer pages

---
+++ E-Bazaar/src/components/comp2/Farmer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './farmer.css'
+
+const Farmer = () => {
+  return (
+    <div className="farmer">
+        <h1>Farmer</h1>
+    </div>
+  )
+}
+
+export default Farmer
---
+++ E-Bazaar/src/components/comp3/Buyer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './buyer.css'
+
+const Buyer = () => {
+  return (
+    <div className="buyer">
+        <h1>Buyer</h1>
+    </div>
+  )
+}
+
+export default Buyer
