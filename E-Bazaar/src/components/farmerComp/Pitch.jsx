c970630fc161c1b6dc9ec707420a1cbe7f4266be
Mohak Chauhan <mohak46812@gmail.com>
2023-03-27 18:52:31 +0530

    styled pitch portal

--- E-Bazaar/src/components/farmerComp/Pitch.jsx
+++ E-Bazaar/src/components/farmerComp/Pitch.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useState} from "react";
 import Axios from "axios";
+import './pitch.css'
 
 const Pitch = () => {
 const [product,setProduct] = useState("");
@@ -22,19 +23,33 @@ const add_product=()=>{
 }
 
   return (
-    <div>
-      <h1>Pitch</h1>
-      <label >product</label>
-      <input type="text" onChange={(e) => setProduct(e.target.value)}/>
-      <label >description</label>
-      <input type="text" onChange={(e) => setdescription(e.target.value)}/>
-      <label >location</label>
-      <input type="text" onChange={(e) => setLocation(e.target.value)}/>
-      <label >quntity_in_kg</label>
-      <input type="text" onChange={(e) => setQuntity_in_kg(e.target.value)}/>
-      <label >price_per_kg</label>
-      <input type="text" onChange={(e) => setPrice_per_kg(e.target.value)}/>
-      <button onClick={add_product}>button</button>
+    <div className='pitch'>
+      <h1>Pitch Your Crop</h1>
+      <div className='part'>
+        <label >PRODUCT</label>
+        <input type="text"  onChange={(e) => setProduct(e.target.value)}/>
+      </div>
+      <div className='part'>
+        <label >DESCRIPTION</label>
+        <textarea name="textarea" placeholder='Give details of your product' id="" cols="30" rows="1"
+          onChange={(e) => setdescription(e.target.value)}>
+        </textarea>
+      </div>
+      <div className='part'>
+        <label >LOCATION</label>
+        <input type="text" onChange={(e) => setLocation(e.target.value)}/>
+      </div>
+      <div className='part'>
+        <label >QUANTITY (KGS)</label>
+        <input type="number" id='number' min='50' onChange={(e) => setQuntity_in_kg(e.target.value)}/>
+      </div>
+      <div className='part'>
+        <label >PRICE (PER KG)</label>
+        <input type="number" id='number' onChange={(e) => setPrice_per_kg(e.target.value)}/>
+      </div>
+      <div className='part'>
+        <button onClick={add_product}>MAKE THE PITCH</button>
+      </div>
     </div>
   )
 }
