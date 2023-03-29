f47bf245b22efd183c6c60d042d89b7521bfc851
Srijan Verma <srijan@Srijans-Air.mshome.net>
2023-03-29 10:46:08 +0530

    change class to className in service.jsx

--- E-Bazaar/src/components/comp1/Services.jsx
+++ E-Bazaar/src/components/comp1/Services.jsx
@@ -17,32 +17,32 @@ const Services = () => {
   return (
     <div id='services' className='service'>
       <div>
-        <h1 class="text-center pt-5">SERVICES</h1>
+        <h1 className="text-center pt-5">SERVICES</h1>
       </div>
       <div className='parent'>
-        <div class="px-3 inner">
-          <div class="card">
-            <h3 class="card-header text-center ">Farmer</h3>
-            <div class="card-body">
-              <h5 class="card-title">Sell Your Crops as Farmer</h5>
+        <div className="px-3 inner">
+          <div className="card">
+            <h3 className="card-header text-center ">Farmer</h3>
+            <div className="card-body">
+              <h5 className="card-title">Sell Your Crops as Farmer</h5>
               <ul>
                 <li>Sell any type of crop that you grow as per your price</li>
                 <li>Fulfill the crop demand as requested by Buyers</li>
               </ul>
-              <button class="btn btn-primary" onClick={farmerhelp}>Sell Your Crop</button>
+              <button className="btn btn-primary" onClick={farmerhelp}>Sell Your Crop</button>
             </div>
           </div>
         </div>
-        <div class="px-3 inner">
-          <div class="card ">
-            <h3 class="card-header text-center">Buyer</h3>
-            <div class="card-body">
-              <h5 class="card-title">Purchase The Crops as Buyer</h5>
+        <div className="px-3 inner">
+          <div className="card ">
+            <h3 className="card-header text-center">Buyer</h3>
+            <div className="card-body">
+              <h5 className="card-title">Purchase The Crops as Buyer</h5>
               <ul>
                 <li>Buy the fresh crops pitched by Farmers</li>
                 <li>Make the pre-order for the crop you want in future</li>
               </ul>
-              <button class="btn btn-primary" onClick={buyerhelp}>Purchase The Crop</button>
+              <button className="btn btn-primary" onClick={buyerhelp}>Purchase The Crop</button>
             </div>
           </div>
         </div>
